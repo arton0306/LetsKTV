b2aa239e4334dd4d595923149c0eb2126c7c66be
arton0306 <arton0306@gmail.com>
2014-03-21 21:32:01 +0800

    add file

---
+++ PageLayoutInfo.hpp
@@ -0,0 +1,10 @@
+#ifndef PAGELAYOUTINFO_HPP
+#define PAGELAYOUTINFO_HPP
+
+namespace SongBook
+{
+    const int TABLE_ROW_COUNT = 18;
+    const int N_SONG_PER_ROW = 2; // we use 3 column to print a song so we don't name this variable a TABLE_COL_COUNT
+}
+
+#endif
